1f3168f7c697dc028831a9011576afa7a16d8848
Leonhard Tissen <contact@warze.org>
2023-11-14 14:52:12 +0100

    Remove unneeded stuff

--- custom.d.ts
+++ custom.d.ts
@@ -2,7 +2,3 @@ declare module '*.png' {
     const value: string;
     export default value;
 }
-declare module '*.mp3' {
-    const value: string;
-    export default value;
-}
--- global.d.ts
+++
@@ -1,9 +0,0 @@
-import { Application, Filter } from 'pixi.js';
-
-declare global {
-	interface Window {
-        __PIXI_APP__: Application
-        tealFilter: Filter
-        resizeEditorLevel: (x: number, y: number) => void
-    }
-}
